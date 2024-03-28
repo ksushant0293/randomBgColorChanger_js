dc9fc1c435e826c02b48d0e21604e3372e48a694
sushant kumar <ksushant0293@gmail.com>
2024-03-28 19:47:21 +0530

    added events on button

---
+++ script.js
@@ -0,0 +1,21 @@
+// generate random color
+
+const randomColor = function(){
+    const hex = "0123456789ABCDEF"
+    let color = "#"
+    for (let i = 0; i < 6; i++) {
+        color += hex[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+const startChaningColor = function(){
+    setInterval(()=>{
+        document.body.style.backgroundColor = randomColor();
+    },1000)
+};
+
+const stopChangingColor = function(){};
+
+document.querySelector("#start").addEventListener("click",startChaningColor);
+
+document.querySelector("#stop").addEventListener("click", stopChangingColor);
